3d78ef72e0a44ed57e8e38db897b082b40c9b5dc
ryusi11 <rstoyo.yuji.maeda@gmail.com>
2023-09-01 18:06:55 +0900

    修正３

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,7 +17,7 @@ class Item < ApplicationRecord
   validates :place_id, presence: true
   validates :delivery_day_id, presence: true
   validates :price, presence: true
-  validates :price, numericality: { in: 300..9999999 }, format: { with: /\A[0-9]+\z/ }
+  validates :price, numericality: { in: 300..9999999, only_integer: true }
   validates :image, presence: true
 
   validates :category_id, :condition_id, :delivery_price_id, :place_id, :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank" } 
